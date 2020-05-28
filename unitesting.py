05d892ee52eb4f813b329af4fbefc8c6ba114b73
nathanforester <nfeugene86@gmail.com>
2020-05-28 22:42:54 +0100

    unit testing changed slightly

--- unitesting.py
+++ unitesting.py
@@ -2,8 +2,8 @@ import unittest
 #from unittest import TestCase
 # from new_bizz_buzz import *
 
-def check_if_digit_div_num(digit, num_div):
-    return digit % num_div == 0
+# def check_if_digit_div_num(digit, num_div):
+#     return digit % num_div == 0
 
 # def assertTrue(arg1, arg2):
 #     print(arg1 == arg2)
@@ -12,7 +12,13 @@ def check_if_digit_div_num(digit, num_div):
 class Tests(unittest.TestCase):
 
     def test(self):
-        self.assertTrue(check_if_digit_div_num(5, 5), True)
+        self.assertTrue(check_if_digit_div_num('bread', 'water'), True)
+        self.assertTrue(check_if_digit_div_num('water', 'bread'), True)
+        self.assertTrue(check_if_digit_div_num('bread', 'bread'), False)
+        self.assertTrue(check_if_digit_div_num('water', 'water'), False)
+        self.assertTrue(check_if_digit_div_num('bread', 'paraffin'), False)
+        self.assertTrue(check_if_digit_div_num('paraffin', 'water'), False)
+        self.assertTrue(check_if_digit_div_num('paraffin', 'paraffin'), True)
         # print(check_if_digit_div_num(5, 5) == True)
         if __name__ == '__main__':
             # print("I was called directly")
